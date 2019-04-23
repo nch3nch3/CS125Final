b04a5f292e4faeb7ce4578fec286e5970714ebb8
nhoffman <nch3@illinois.edu>
2019-04-23 12:50:10 -0500

    Initial Setup + UI basics + UI testing + UI toDo

--- app/src/main/java/com/example/spacexlaunchtracker/MainActivity.java
+++ app/src/main/java/com/example/spacexlaunchtracker/MainActivity.java
@@ -19,25 +19,30 @@ public class MainActivity extends AppCompatActivity {
         final Button latest = findViewById(R.id.latestLaunch);
         latest.setOnClickListener(v -> {
             Log.d(TAG, "latest launch button clicked");
-            updateText();
+            updateText('l');
         });
         final Button next = findViewById(R.id.nextLaunch);
         next.setOnClickListener(v -> {
             Log.d(TAG, "Next Launch button clicked");
-            updateText();
+            updateText('n');
         });
         final Button random = findViewById(R.id.randomLaunch);
         random.setOnClickListener(v -> {
             Log.d(TAG, "Random Launch button clicked");
-            updateText();
+            updateText('r');
         });
+        //These function handle the buttons
+
+        //Can you write a function that takes care of the "launchFinder" button and the Flight Mission field
     }
-    public void updateText() {
+    public void updateText(final char inputCase) {
+        //this function handles random, next, and latest launch via characters which represent each case
         double randomNumber = Math.random();
         String caption = ("This is a number for testing: " + randomNumber);
         ((TextView) findViewById(R.id.caption)).setText(caption);
+        //This is for testing code is for testing
     }
     public void updateText(final int input) {
-
+        //This method takes the flight number from the plainText field.
     }
 }
