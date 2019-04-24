5c0d4f011f076531181ff6fe65c70d7b4fa68e6f
nhoffman <nch3@illinois.edu>
2019-04-24 17:45:03 -0500

    Initial Setup + UI basics + UI testing + UI Basics done + GSON/VOLLEY + Tasks/Import

---
+++ app/src/main/java/com/example/spacexlaunchtracker/Tasks.java
@@ -0,0 +1,12 @@
+package com.example.spacexlaunchtracker;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.RequestQueue;
+
+public class Tasks {
+}
